43205760799b57f557426649f53119e1aa8d6a32
Alexey Rybakov <rybakov@jscc.ru>
2022-09-20 10:41:48 +0300

    conditions for calculations

--- src/ibm.cpp
+++ src/ibm.cpp
@@ -601,13 +601,17 @@ d_to_u()
 {
     LOOP1
     {
-        ru[i] = r[i] * u[i];
-        rv[i] = r[i] * v[i];
-        rw[i] = r[i] * w[i];
+        // В потоках участвуют обычные и фиктивные ячейки.
+        if ((kind[i] == KIND_COMMON) || (kind[i] == KIND_GHOST))
+        {
+            ru[i] = r[i] * u[i];
+            rv[i] = r[i] * v[i];
+            rw[i] = r[i] * w[i];
 
-        // E = rho * (V^2/2 + e) = rho * (V^2/2 + p/((GAMMA - 1.0) * rho))
-        //   = rho * V^2/2 + p/(GAMMA - 1)
-        E[i] = 0.5 * r[i] * (u[i] * u[i] + v[i] * v[i] + w[i] * w[i]) + p[i] / (GAMMA - 1.0);
+            // E = rho * (V^2/2 + e) = rho * (V^2/2 + p/((GAMMA - 1.0) * rho))
+            //   = rho * V^2/2 + p/(GAMMA - 1)
+            E[i] = 0.5 * r[i] * (u[i] * u[i] + v[i] * v[i] + w[i] * w[i]) + p[i] / (GAMMA - 1.0);
+        }
     }
 }
 
@@ -617,13 +621,17 @@ u_to_d()
 {
     LOOP1
     {
-        u[i] = ru[i] / r[i];
-        v[i] = rv[i] / r[i];
-        w[i] = rw[i] / r[i];
+        // В основные величины пересчитываем только обычные ячейки.
+        if (kind[i] == KIND_COMMON)
+        {
+            u[i] = ru[i] / r[i];
+            v[i] = rv[i] / r[i];
+            w[i] = rw[i] / r[i];
 
-        // E = rho * V^2/2 + p/(GAMMA - 1.0)
-        // p = (E - rho * V^2/2) * (GAMMA - 1.0)
-        p[i] = (E[i] - 0.5 * r[i] * (u[i] * u[i] + v[i] * v[i] + w[i] * w[i])) * (GAMMA - 1.0);
+            // E = rho * V^2/2 + p/(GAMMA - 1.0)
+            // p = (E - rho * V^2/2) * (GAMMA - 1.0)
+            p[i] = (E[i] - 0.5 * r[i] * (u[i] * u[i] + v[i] * v[i] + w[i] * w[i])) * (GAMMA - 1.0);
+        }
     }
 }
 
@@ -633,31 +641,34 @@ calc_f()
 {
     LOOP1
     {
-        double a = sqrt(GAMMA * p[i] / r[i]);
-        double l1 = u[i] - a;
-        double l2 = u[i];
-        double l5 = u[i] + a;
-        double lp1 = 0.5 * (l1 + abs(l1));
-        double lp2 = 0.5 * (l2 + abs(l2));
-        double lp5 = 0.5 * (l5 + abs(l5));
-        double ln1 = 0.5 * (l1 - abs(l1));
-        double ln2 = 0.5 * (l2 - abs(l2));
-        double ln5 = 0.5 * (l5 - abs(l5));
-        double k = 0.5 * r[i] / GAMMA;
-        double V2 = u[i] * u[i] + v[i] * v[i] + w[i] * w[i];
-        double H = 0.5 * V2 + a * a / (GAMMA - 1.0);
+        if ((kind[i] == KIND_COMMON) || (kind[i] == KIND_GHOST))
+        {
+            double a = sqrt(GAMMA * p[i] / r[i]);
+            double l1 = u[i] - a;
+            double l2 = u[i];
+            double l5 = u[i] + a;
+            double lp1 = 0.5 * (l1 + abs(l1));
+            double lp2 = 0.5 * (l2 + abs(l2));
+            double lp5 = 0.5 * (l5 + abs(l5));
+            double ln1 = 0.5 * (l1 - abs(l1));
+            double ln2 = 0.5 * (l2 - abs(l2));
+            double ln5 = 0.5 * (l5 - abs(l5));
+            double k = 0.5 * r[i] / GAMMA;
+            double V2 = u[i] * u[i] + v[i] * v[i] + w[i] * w[i];
+            double H = 0.5 * V2 + a * a / (GAMMA - 1.0);
 
-        fp_r[i] = k * (lp1 + 2.0 * (GAMMA - 1.0) * lp2 + lp5);
-        fp_ru[i] = k * ((u[i] - a) * lp1 + 2.0 * (GAMMA - 1.0) * u[i] * lp2 + (u[i] + a) * lp5);
-        fp_rv[i] = k * (v[i] * lp1 + 2.0 * (GAMMA - 1.0) * v[i] * lp2 + v[i] * lp5);
-        fp_rw[i] = k * (w[i] * lp1 + 2.0 * (GAMMA - 1.0) * w[i] * lp2 + w[i] * lp5);
-        fp_E[i] = k * ((H - u[i] * a) * lp1 + (GAMMA - 1.0) * V2 * lp2 + (H + u[i] * a) * lp5);
+            fp_r[i] = k * (lp1 + 2.0 * (GAMMA - 1.0) * lp2 + lp5);
+            fp_ru[i] = k * ((u[i] - a) * lp1 + 2.0 * (GAMMA - 1.0) * u[i] * lp2 + (u[i] + a) * lp5);
+            fp_rv[i] = k * (v[i] * lp1 + 2.0 * (GAMMA - 1.0) * v[i] * lp2 + v[i] * lp5);
+            fp_rw[i] = k * (w[i] * lp1 + 2.0 * (GAMMA - 1.0) * w[i] * lp2 + w[i] * lp5);
+            fp_E[i] = k * ((H - u[i] * a) * lp1 + (GAMMA - 1.0) * V2 * lp2 + (H + u[i] * a) * lp5);
 
-        fn_r[i] = k * (ln1 + 2.0 * (GAMMA - 1.0) * ln2 + ln5);
-        fn_ru[i] = k * ((u[i] - a) * ln1 + 2.0 * (GAMMA - 1.0) * u[i] * ln2 + (u[i] + a) * ln5);
-        fn_rv[i] = k * (v[i] * ln1 + 2.0 * (GAMMA - 1.0) * v[i] * ln2 + v[i] * ln5);
-        fn_rw[i] = k * (w[i] * ln1 + 2.0 * (GAMMA - 1.0) * w[i] * ln2 + w[i] * ln5);
-        fn_E[i] = k * ((H - u[i] * a) * ln1 + (GAMMA - 1.0) * V2 * ln2 + (H + u[i] * a) * ln5);
+            fn_r[i] = k * (ln1 + 2.0 * (GAMMA - 1.0) * ln2 + ln5);
+            fn_ru[i] = k * ((u[i] - a) * ln1 + 2.0 * (GAMMA - 1.0) * u[i] * ln2 + (u[i] + a) * ln5);
+            fn_rv[i] = k * (v[i] * ln1 + 2.0 * (GAMMA - 1.0) * v[i] * ln2 + v[i] * ln5);
+            fn_rw[i] = k * (w[i] * ln1 + 2.0 * (GAMMA - 1.0) * w[i] * ln2 + w[i] * ln5);
+            fn_E[i] = k * ((H - u[i] * a) * ln1 + (GAMMA - 1.0) * V2 * ln2 + (H + u[i] * a) * ln5);
+        }
     }
 }
 
@@ -667,31 +678,34 @@ calc_g()
 {
     LOOP1
     {
-        double a = sqrt(GAMMA * p[i] / r[i]);
-        double l1 = v[i] - a;
-        double l2 = v[i];
-        double l5 = v[i] + a;
-        double lp1 = 0.5 * (l1 + abs(l1));
-        double lp2 = 0.5 * (l2 + abs(l2));
-        double lp5 = 0.5 * (l5 + abs(l5));
-        double ln1 = 0.5 * (l1 - abs(l1));
-        double ln2 = 0.5 * (l2 - abs(l2));
-        double ln5 = 0.5 * (l5 - abs(l5));
-        double k = 0.5 * r[i] / GAMMA;
-        double V2 = u[i] * u[i] + v[i] * v[i] + w[i] * w[i];
-        double H = 0.5 * V2 + a * a / (GAMMA - 1.0);
+        if ((kind[i] == KIND_COMMON) || (kind[i] == KIND_GHOST))
+        {
+            double a = sqrt(GAMMA * p[i] / r[i]);
+            double l1 = v[i] - a;
+            double l2 = v[i];
+            double l5 = v[i] + a;
+            double lp1 = 0.5 * (l1 + abs(l1));
+            double lp2 = 0.5 * (l2 + abs(l2));
+            double lp5 = 0.5 * (l5 + abs(l5));
+            double ln1 = 0.5 * (l1 - abs(l1));
+            double ln2 = 0.5 * (l2 - abs(l2));
+            double ln5 = 0.5 * (l5 - abs(l5));
+            double k = 0.5 * r[i] / GAMMA;
+            double V2 = u[i] * u[i] + v[i] * v[i] + w[i] * w[i];
+            double H = 0.5 * V2 + a * a / (GAMMA - 1.0);
 
-        gp_r[i] = k * (lp1 + 2.0 * (GAMMA - 1.0) * lp2 + lp5);
-        gp_ru[i] = k * (u[i] * lp1 + 2.0 * (GAMMA - 1.0) * u[i] * lp2 + u[i] * lp5);
-        gp_rv[i] = k * ((v[i] - a) * lp1 + 2.0 * (GAMMA - 1.0) * v[i] * lp2 + (v[i] + a) * lp5);
-        gp_rw[i] = k * (w[i] * lp1 + 2.0 * (GAMMA - 1.0) * w[i] * lp2 + w[i] * lp5);
-        gp_E[i] = k * ((H - v[i] * a) * lp1 + (GAMMA - 1.0) * V2 * lp2 + (H + v[i] * a) * lp5);
+            gp_r[i] = k * (lp1 + 2.0 * (GAMMA - 1.0) * lp2 + lp5);
+            gp_ru[i] = k * (u[i] * lp1 + 2.0 * (GAMMA - 1.0) * u[i] * lp2 + u[i] * lp5);
+            gp_rv[i] = k * ((v[i] - a) * lp1 + 2.0 * (GAMMA - 1.0) * v[i] * lp2 + (v[i] + a) * lp5);
+            gp_rw[i] = k * (w[i] * lp1 + 2.0 * (GAMMA - 1.0) * w[i] * lp2 + w[i] * lp5);
+            gp_E[i] = k * ((H - v[i] * a) * lp1 + (GAMMA - 1.0) * V2 * lp2 + (H + v[i] * a) * lp5);
 
-        gn_r[i] = k * (ln1 + 2.0 * (GAMMA - 1.0) * ln2 + ln5);
-        gn_ru[i] = k * (u[i] * ln1 + 2.0 * (GAMMA - 1.0) * u[i] * ln2 + u[i] * ln5);
-        gn_rv[i] = k * ((v[i] - a) * ln1 + 2.0 * (GAMMA - 1.0) * v[i] * ln2 + (v[i] + a) * ln5);
-        gn_rw[i] = k * (w[i] * ln1 + 2.0 * (GAMMA - 1.0) * w[i] * ln2 + w[i] * ln5);
-        gn_E[i] = k * ((H - v[i] * a) * ln1 + (GAMMA - 1.0) * V2 * ln2 + (H + v[i] * a) * ln5);
+            gn_r[i] = k * (ln1 + 2.0 * (GAMMA - 1.0) * ln2 + ln5);
+            gn_ru[i] = k * (u[i] * ln1 + 2.0 * (GAMMA - 1.0) * u[i] * ln2 + u[i] * ln5);
+            gn_rv[i] = k * ((v[i] - a) * ln1 + 2.0 * (GAMMA - 1.0) * v[i] * ln2 + (v[i] + a) * ln5);
+            gn_rw[i] = k * (w[i] * ln1 + 2.0 * (GAMMA - 1.0) * w[i] * ln2 + w[i] * ln5);
+            gn_E[i] = k * ((H - v[i] * a) * ln1 + (GAMMA - 1.0) * V2 * ln2 + (H + v[i] * a) * ln5);
+        }
     }
 }
 
@@ -701,31 +715,34 @@ calc_h()
 {
     LOOP1
     {
-        double a = sqrt(GAMMA * p[i] / r[i]);
-        double l1 = w[i] - a;
-        double l2 = w[i];
-        double l5 = w[i] + a;
-        double lp1 = 0.5 * (l1 + abs(l1));
-        double lp2 = 0.5 * (l2 + abs(l2));
-        double lp5 = 0.5 * (l5 + abs(l5));
-        double ln1 = 0.5 * (l1 - abs(l1));
-        double ln2 = 0.5 * (l2 - abs(l2));
-        double ln5 = 0.5 * (l5 - abs(l5));
-        double k = 0.5 * r[i] / GAMMA;
-        double V2 = u[i] * u[i] + v[i] * v[i] + w[i] * w[i];
-        double H = 0.5 * V2 + a * a / (GAMMA - 1.0);
+        if ((kind[i] == KIND_COMMON) || (kind[i] == KIND_GHOST))
+        {
+            double a = sqrt(GAMMA * p[i] / r[i]);
+            double l1 = w[i] - a;
+            double l2 = w[i];
+            double l5 = w[i] + a;
+            double lp1 = 0.5 * (l1 + abs(l1));
+            double lp2 = 0.5 * (l2 + abs(l2));
+            double lp5 = 0.5 * (l5 + abs(l5));
+            double ln1 = 0.5 * (l1 - abs(l1));
+            double ln2 = 0.5 * (l2 - abs(l2));
+            double ln5 = 0.5 * (l5 - abs(l5));
+            double k = 0.5 * r[i] / GAMMA;
+            double V2 = u[i] * u[i] + v[i] * v[i] + w[i] * w[i];
+            double H = 0.5 * V2 + a * a / (GAMMA - 1.0);
 
-        gp_r[i] = k * (lp1 + 2.0 * (GAMMA - 1.0) * lp2 + lp5);
-        gp_ru[i] = k * (u[i] * lp1 + 2.0 * (GAMMA - 1.0) * u[i] * lp2 + u[i] * lp5);
-        gp_rv[i] = k * (v[i] * lp1 + 2.0 * (GAMMA - 1.0) * v[i] * lp2 + v[i] * lp5);
-        gp_rw[i] = k * ((w[i] - a) * lp1 + 2.0 * (GAMMA - 1.0) * w[i] * lp2 + (w[i] + a) * lp5);
-        gp_E[i] = k * ((H - w[i] * a) * lp1 + (GAMMA - 1.0) * V2 * lp2 + (H + w[i] * a) * lp5);
+            gp_r[i] = k * (lp1 + 2.0 * (GAMMA - 1.0) * lp2 + lp5);
+            gp_ru[i] = k * (u[i] * lp1 + 2.0 * (GAMMA - 1.0) * u[i] * lp2 + u[i] * lp5);
+            gp_rv[i] = k * (v[i] * lp1 + 2.0 * (GAMMA - 1.0) * v[i] * lp2 + v[i] * lp5);
+            gp_rw[i] = k * ((w[i] - a) * lp1 + 2.0 * (GAMMA - 1.0) * w[i] * lp2 + (w[i] + a) * lp5);
+            gp_E[i] = k * ((H - w[i] * a) * lp1 + (GAMMA - 1.0) * V2 * lp2 + (H + w[i] * a) * lp5);
 
-        gn_r[i] = k * (ln1 + 2.0 * (GAMMA - 1.0) * ln2 + ln5);
-        gn_ru[i] = k * (u[i] * ln1 + 2.0 * (GAMMA - 1.0) * u[i] * ln2 + u[i] * ln5);
-        gn_rv[i] = k * (v[i] * ln1 + 2.0 * (GAMMA - 1.0) * v[i] * ln2 + v[i] * ln5);
-        gn_rw[i] = k * ((w[i] - a) * ln1 + 2.0 * (GAMMA - 1.0) * w[i] * ln2 + (w[i] + a) * ln5);
-        gn_E[i] = k * ((H - w[i] * a) * ln1 + (GAMMA - 1.0) * V2 * ln2 + (H + w[i] * a) * ln5);
+            gn_r[i] = k * (ln1 + 2.0 * (GAMMA - 1.0) * ln2 + ln5);
+            gn_ru[i] = k * (u[i] * ln1 + 2.0 * (GAMMA - 1.0) * u[i] * ln2 + u[i] * ln5);
+            gn_rv[i] = k * (v[i] * ln1 + 2.0 * (GAMMA - 1.0) * v[i] * ln2 + v[i] * ln5);
+            gn_rw[i] = k * ((w[i] - a) * ln1 + 2.0 * (GAMMA - 1.0) * w[i] * ln2 + (w[i] + a) * ln5);
+            gn_E[i] = k * ((H - w[i] * a) * ln1 + (GAMMA - 1.0) * V2 * ln2 + (H + w[i] * a) * ln5);
+        }
     }
 }
 
@@ -749,79 +766,77 @@ calc_flows()
         int i = LIN(ix, iy, iz);
 
         // Вычисления производятся только для COMMON ячеек.
-        if (kind[i] != KIND_COMMON)
+        if (kind[i] == KIND_COMMON)
         {
-            continue;
+            //
+            // Направление X.
+            //
+
+            int li = i;
+            int ri = i;
+
+            if (ix > 0)
+            {
+                li = LIN(ix - 1, iy, iz);
+            }
+
+            if (ix < NX - 1)
+            {
+                ri = LIN(ix + 1, iy, iz);
+            }
+
+            r[i] -= (DT / DH) * (fp_r[i] + fn_r[ri] - fp_r[li] - fn_r[i]);
+            ru[i] -= (DT / DH) * (fp_ru[i] + fn_ru[ri] - fp_ru[li] - fn_ru[i]);
+            rv[i] -= (DT / DH) * (fp_rv[i] + fn_rv[ri] - fp_rv[li] - fn_rv[i]);
+            rw[i] -= (DT / DH) * (fp_rw[i] + fn_rw[ri] - fp_rw[li] - fn_rw[i]);
+            E[i] -= (DT / DH) * (fp_E[i] + fn_E[ri] - fp_E[li] - fn_E[i]);
+
+            //
+            // Направление Y.
+            //
+
+            li = i;
+            ri = i;
+
+            if (iy > 0)
+            {
+                li = LIN(ix, iy - 1, iz);
+            }
+
+            if (iy < NY - 1)
+            {
+                ri = LIN(ix, iy + 1, iz);
+            }
+
+            r[i] -= (DT / DH) * (gp_r[i] + gn_r[ri] - gp_r[li] - gn_r[i]);
+            ru[i] -= (DT / DH) * (gp_ru[i] + gn_ru[ri] - gp_ru[li] - gn_ru[i]);
+            rv[i] -= (DT / DH) * (gp_rv[i] + gn_rv[ri] - gp_rv[li] - gn_rv[i]);
+            rw[i] -= (DT / DH) * (gp_rw[i] + gn_rw[ri] - gp_rw[li] - gn_rw[i]);
+            E[i] -= (DT / DH) * (gp_E[i] + gn_E[ri] - gp_E[li] - gn_E[i]);
+
+            //
+            // Направление Z.
+            //
+
+            li = i;
+            ri = i;
+
+            if (iz > 0)
+            {
+                li = LIN(ix, iy, iz - 1);
+            }
+
+            if (iz < NZ - 1)
+            {
+                ri = LIN(ix, iy, iz + 1);
+            }
+
+            r[i] -= (DT / DH) * (hp_r[i] + hn_r[ri] - hp_r[li] - hn_r[i]);
+            ru[i] -= (DT / DH) * (hp_ru[i] + hn_ru[ri] - hp_ru[li] - hn_ru[i]);
+            rv[i] -= (DT / DH) * (hp_rv[i] + hn_rv[ri] - hp_rv[li] - hn_rv[i]);
+            rw[i] -= (DT / DH) * (hp_rw[i] + hn_rw[ri] - hp_rw[li] - hn_rw[i]);
+            E[i] -= (DT / DH) * (hp_E[i] + hn_E[ri] - hp_E[li] - hn_E[i]);
         }
-
-        //
-        // Направление X.
-        //
-
-        int li = i;
-        int ri = i;
-
-        if (ix > 0)
-        {
-            li = LIN(ix - 1, iy, iz);
-        }
-
-        if (ix < NX - 1)
-        {
-            ri = LIN(ix + 1, iy, iz);
-        }
-
-        r[i] -= (DT / DH) * (fp_r[i] + fn_r[ri] - fp_r[li] - fn_r[i]);
-        ru[i] -= (DT / DH) * (fp_ru[i] + fn_ru[ri] - fp_ru[li] - fn_ru[i]);
-        rv[i] -= (DT / DH) * (fp_rv[i] + fn_rv[ri] - fp_rv[li] - fn_rv[i]);
-        rw[i] -= (DT / DH) * (fp_rw[i] + fn_rw[ri] - fp_rw[li] - fn_rw[i]);
-        E[i] -= (DT / DH) * (fp_E[i] + fn_E[ri] - fp_E[li] - fn_E[i]);
-
-        //
-        // Направление Y.
-        //
-
-        li = i;
-        ri = i;
-
-        if (iy > 0)
-        {
-            li = LIN(ix, iy - 1, iz);
-        }
-
-        if (iy < NY - 1)
-        {
-            ri = LIN(ix, iy + 1, iz);
-        }
-
-        r[i] -= (DT / DH) * (gp_r[i] + gn_r[ri] - gp_r[li] - gn_r[i]);
-        ru[i] -= (DT / DH) * (gp_ru[i] + gn_ru[ri] - gp_ru[li] - gn_ru[i]);
-        rv[i] -= (DT / DH) * (gp_rv[i] + gn_rv[ri] - gp_rv[li] - gn_rv[i]);
-        rw[i] -= (DT / DH) * (gp_rw[i] + gn_rw[ri] - gp_rw[li] - gn_rw[i]);
-        E[i] -= (DT / DH) * (gp_E[i] + gn_E[ri] - gp_E[li] - gn_E[i]);
-
-        //
-        // Направление Z.
-        //
-
-        li = i;
-        ri = i;
-
-        if (iz > 0)
-        {
-            li = LIN(ix, iy, iz - 1);
-        }
-
-        if (iz < NZ - 1)
-        {
-            ri = LIN(ix, iy, iz + 1);
-        }
-
-        r[i] -= (DT / DH) * (hp_r[i] + hn_r[ri] - hp_r[li] - hn_r[i]);
-        ru[i] -= (DT / DH) * (hp_ru[i] + hn_ru[ri] - hp_ru[li] - hn_ru[i]);
-        rv[i] -= (DT / DH) * (hp_rv[i] + hn_rv[ri] - hp_rv[li] - hn_rv[i]);
-        rw[i] -= (DT / DH) * (hp_rw[i] + hn_rw[ri] - hp_rw[li] - hn_rw[i]);
-        E[i] -= (DT / DH) * (hp_E[i] + hn_E[ri] - hp_E[li] - hn_E[i]);
     }
 }
 
